3d701207e136890d17842934aab17565c44cff62
Ademar leal lisboa <ademar.lisboa@gmail.com>
2021-10-28 11:50:41 -0300

    bind campos usuario e senha na tela de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,  } from '@angular/core';
 import {  MenuController, NavController } from '@ionic/angular'
+import { CredenciaisDto } from 'src/models/credenciaisDto';
 
 //import { runInThisContext } from 'vm';
 
@@ -12,13 +13,20 @@ import {  MenuController, NavController } from '@ionic/angular'
 export class LoginPage implements OnInit {
 
   constructor(public navCtrl: NavController, public menu: MenuController) { }
-
+  
+  creds : CredenciaisDto =  {
+    email: "",
+    senha: ""
+  };
+  
   ngOnInit() {
      
 
   }
 
   public login(){
+    
+    alert(this.creds.email);
     this.navCtrl.navigateForward('/home');
   }
 
---
+++ src/models/credenciaisDto.ts
@@ -0,0 +1,4 @@
+export interface CredenciaisDto{
+    email: String;
+    senha: String;
+}
